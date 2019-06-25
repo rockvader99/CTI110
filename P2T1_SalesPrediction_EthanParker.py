3976075462b5a3cd27444bf8932da4c7489992fb
rockvader99 <52185801+rockvader99@users.noreply.github.com>
2019-06-24 22:26:09 -0400

    Add files via upload
    First file to Portfolio; Calculates Sales.

---
+++ P2T1_SalesPrediction_EthanParker.py
@@ -0,0 +1,11 @@
+# This Project Calculates profit as 23 percent of total sales
+# 6/18/2019
+# CTI-110 P2T1- Sales Prediciton
+# Ethan Parker
+#
+total_sales = float( input('Enter the Projected Sales: ') )
+
+profit = total_sales * 0.23
+
+print('The Profit is $', profit, format(profit, ',.2f'))
+
